215a5623b93eee58f56cdf467e7403b6773cc210
Kauasm <kauasm2017@gmail.com>
2024-11-03 22:29:30 -0300

    first commit

---
+++ src/main/java/com/GestaoCompraPeixe/GestaoPesca/GestaoPescaApplication.java
@@ -0,0 +1,13 @@
+package com.GestaoCompraPeixe.GestaoPesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPescaApplication.class, args);
+	}
+
+}
